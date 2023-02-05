0bfaf874baea9721b23f91413820f508fb810998
Derevenskikh Aleksander <hedgehog@kartis.ru>
2023-02-05 16:33:17 +0300

    Initial Commit

---
+++ CodingStyleCased/CodingStyleCased.h
@@ -0,0 +1,18 @@
+//
+//  CodingStyleCased.h
+//  CodingStyleCased
+//
+//  Created by Aleksandr Derevenskih on 05.02.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CodingStyleCased.
+FOUNDATION_EXPORT double CodingStyleCasedVersionNumber;
+
+//! Project version string for CodingStyleCased.
+FOUNDATION_EXPORT const unsigned char CodingStyleCasedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CodingStyleCased/PublicHeader.h>
+
+
